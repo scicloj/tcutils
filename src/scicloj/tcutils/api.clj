4929f053c91345ebfd9e6a6943141080cbf0289e
Kira McLean <email@kiramclean.com>
2024-02-28 22:30:27 -0400

    add beginnings of lead, cumsum, and substract-columns

--- src/scicloj/tcutils/api.clj
+++ src/scicloj/tcutils/api.clj
@@ -1,17 +1,20 @@
 (ns scicloj.tcutils.api
-  (:require [tech.v3.dataset.rolling :as rolling]))
+  (:require [tech.v3.dataset.rolling :as rolling]
+            [tech.v3.datatype.functional :as fun]
+            [tablecloth.api :as tc]))
 
 (defn lag
   "Compute previous (lagged) values from one column in a new column, used to compare values ahead of the current value.
 
   ## Usage
 
+  `lag(ds column-selector lag-size)`
   `lag(ds new-column-name column-selector lag-size)`
 
   ## Arguments
 
   | ds | A `tech.ml.dataset` (i.e a `tablecloth` dataset) |
-  | new-column-name | Name for the column where newly computed values will go |
+  | new-column-name | __optional__ Name for the column where newly computed values will go. When ommitted new column name defaults to the keyword `<old-column-name>-lag-<lag-size>`|
   | column-selector | Name of the column to use to compute the lagged values |
   | lag-size | positive integer indicating how many rows to skip over to compute the lag |
 
@@ -19,10 +22,46 @@
 
   A dataset with the column populated with the lagged values.
   "
-  [ds new-column-name column-selector lag-size]
-  (rolling/rolling ds
-                   {:window-type :fixed
-                    :window-size (inc lag-size)
-                    :relative-window-position :left
-                    :edge-mode :zero}
-                   {new-column-name (rolling/first column-selector)}))
+  ([ds column-name lag-size]
+   (let [new-column-name (-> column-name
+                             name
+                             (str "-lag-" lag-size)
+                             keyword)]
+     (lag ds new-column-name column-name lag-size)))
+  ([ds new-column-name column-name lag-size]
+   (rolling/rolling ds
+                    {:window-type :fixed
+                     :window-size (inc lag-size)
+                     :relative-window-position :left
+                     :edge-mode :zero}
+                    {new-column-name (rolling/first column-name)})))
+
+(defn lead
+  " "
+  ([ds column-name lead-size]
+   (let [new-column-name (-> column-name
+                             name
+                             (str "-lead-" lead-size)
+                             keyword)]
+     (lead ds new-column-name column-name lead-size)))
+  ([ds new-column-name column-name lead-size]
+   (rolling/rolling ds
+                    {:window-type :fixed
+                     :window-size (inc lead-size)
+                     :relative-window-position :right
+                     :edge-mode :zero}
+                    {new-column-name (rolling/last column-name)})))
+
+(defn cumsum
+  "Compute the cumulative sum of a column"
+  ([ds column-name]
+   (let [new-column-name (-> column-name
+                             name
+                             (str "-cumulative-sum")
+                             keyword)]
+     (cumsum ds new-column-name column-name)))
+  ([ds new-column-name column-name]
+   (rolling/expanding ds {new-column-name (rolling/sum column-name)})))
+
+(defn substract-columns [ds new-column-name col1 col2]
+  (tc/map-columns new-column-name [col1 col2] fun/-))
--- test/scicloj/tcutils/api_test.clj
+++ test/scicloj/tcutils/api_test.clj
@@ -4,7 +4,7 @@
             [scicloj.tcutils.api :as sut]))
 
 (deftest lag-test
-  (testing "with valid input"
+  (testing "with valid arguments"
     (testing "fills in 0 for lagging numeric values"
       (is (= {:C1 [1 2 3 4 5]
               :lag-1 [0 1 2 3 4]}
@@ -19,9 +19,55 @@
                  (sut/lag :lag-1 :C1 3)
                  (tc/columns :as-map)))))
 
+    (testing "generates new column name if not given one"
+      (is (= {:C1 [1 2 3 4 5]
+              :C1-lag-2 [0 0 1 2 3]}
+             (-> (tc/dataset {:C1 [1 2 3 4 5]})
+                 (sut/lag :C1 2)
+                 (tc/columns :as-map)))))
+
     (testing "fills in `nil` for lagging non-numeric values"
       (is (= {:C1 ["a" "b" "c" "d"]
               :lag-1 [nil "a" "b" "c"]}
              (-> (tc/dataset {:C1 ["a" "b" "c" "d"]})
                  (sut/lag :lag-1 :C1 1)
+                 (tc/columns :as-map)))))))
+
+(deftest lead-test
+  (testing "with valid-input"
+    (testing "fills in 0 for leading numeric values"
+      (is (= {:C1 [1 2 3 4 5]
+              :lead-1 [2 3 4 5 0]}
+             (-> (tc/dataset {:C1 [1 2 3 4 5]})
+                 (sut/lead :lead-1 :C1 1)
+                 (tc/columns :as-map)))))
+
+    (testing "generates new column name if not given one"
+      (is (= {:C1 [1 2 3 4 5]
+              :C1-lead-2 [3 4 5 0 0]}
+             (-> (tc/dataset {:C1 [1 2 3 4 5]})
+                 (sut/lead :C1 2)
+                 (tc/columns :as-map)))))
+
+    (testing "fills in `nil` for leading non-numeric values"
+      (is (= {:C1 ["a" "b" "c" "d" "e"]
+              :lead-3 ["d" "e" nil nil nil]}
+             (-> (tc/dataset {:C1 ["a" "b" "c" "d" "e"]})
+                 (sut/lead :lead-3 :C1 3)
+                 (tc/columns :as-map)))))))
+
+(deftest cumsum-test
+  (testing "with valid inputs"
+    (testing "calculates the cumulative sum of a column in a new named column"
+      (is (= {:C1 [1 2 3 4 5]
+              :sum [1.0 3.0 6.0 10.0 15.0]}
+             (-> (tc/dataset {:C1 [1 2 3 4 5]})
+                 (sut/cumsum :sum :C1)
+                 (tc/columns :as-map)))))
+
+    (testing "generates new column name if not given one"
+      (is (= {:C1 [1 2 3 4 5]
+              :C1-cumulative-sum [1.0 3.0 6.0 10.0 15.0]}
+             (-> (tc/dataset {:C1 [1 2 3 4 5]})
+                 (sut/cumsum :C1)
                  (tc/columns :as-map)))))))
